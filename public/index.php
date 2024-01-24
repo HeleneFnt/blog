83a323b8f384d1df7c85cb40c9849bf979385b6f
LNWatta <helene.finot@le-campus-numerique.fr>
2024-01-24 15:37:17 +0100

    2.1

--- public/index.php
+++ public/index.php
@@ -4,3 +4,4 @@ echo 'Bienvenue sur le Blog';
 $userAction = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_URL);
 
 include ('../config/database.php');
+include ('../app/controllers/homeController.php');
